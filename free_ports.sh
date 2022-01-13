29a9b16bacd5b6ad33455b61afdcbddea8da045b
ALFinternet <ALFinternet@gmail.com>
2022-01-13 15:06:12 -0800

    Update free_ports.sh

--- free_ports.sh
+++ free_ports.sh
@@ -1,19 +1,44 @@
 #! /bin/bash
+
+# NEWLY ADDED BACKUP FUNCTIONALITY IS NOT FULLY TESTED YET, USE WITH CARE, ESPECIALLY DELETION
+# Developed for DSM 6 - 7.0.1. Not tested on other versions.
+# Steps to install:
+# Save this script in one of your shares
+# Edit it according to your requirements
+# Backup /usr/syno/share/nginx/ as follows:
+# Run this script as root
+# Reboot and ensure everything is still working
+# If not, restore the backup and post a comment on this script's gist page
+# If it did, schedule it to run as root at boot through Control Panel -> Task Scheduler
+#
 # find files with 443 in them: grep 443 /usr/syno/share/nginx/*.mustache
 # view in use ports: netstat -plnt | grep ":443"
 # 
-# idea from: https://gist.github.com/hjbotha/f64ef2e0cd1e8ba5ec526dcd6e937dd7
-# modified from this one, because it fails when ran a 2nd time: https://www.reddit.com/r/synology/comments/ahs3xh/prevent_dsm_listening_on_port_80443/
-#
-NEW_HTTP_PORT=80
-NEW_HTTPS_PORT=443
+# stolen from from: https://gist.github.com/hjbotha/f64ef2e0cd1e8ba5ec526dcd6e937dd7
+# additional info limiting files required to be modified: https://www.reddit.com/r/synology/comments/ahs3xh/prevent_dsm_listening_on_port_80443/
 
-#sed -i -e 's/80/81/' -e 's/443/444/' /usr/syno/share/nginx/server.mustache /usr/syno/share/nginx/DSM.mustache /usr/syno/share/nginx/WWWService.mustache
-#sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)80\([^0-9]\)/\1$NEW_HTTP_PORT\2/" /usr/syno/share/nginx/*.mustache
-#sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)443\([^0-9]\)/\1$NEW_HTTPS_PORT\2/" /usr/syno/share/nginx/*.mustache
-sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)80\([^0-9]\)/\1$NEW_HTTP_PORT\2/" /usr/syno/share/nginx/server.mustache /usr/syno/share/nginx/DSM.mustache /usr/syno/share/nginx/WWWService.mustache
-sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)443\([^0-9]\)/\1$NEW_HTTPS_PORT\2/" /usr/syno/share/nginx/server.mustache /usr/syno/share/nginx/DSM.mustache /usr/syno/share/nginx/WWWService.mustache
+HTTP_PORT=5080
+HTTPS_PORT=50443
 
+BACKUP_FILES=true # change to false to disable backups
+BACKUP_DIR=/volume1/syno-scripts/free_ports/backups
+DELETE_OLD_BACKUPS=false # change to true to automatically delete old backups.
+KEEP_BACKUP_DAYS=90
+
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
+CURRENT_BACKUP_DIR="$BACKUP_DIR/$DATE"
+
+if [ "$BACKUP_FILES" == "true" ]; then
+  mkdir -p "$CURRENT_BACKUP_DIR"
+  cp /usr/syno/share/nginx/*.mustache "$CURRENT_BACKUP_DIR"
+fi
+
+if [ "$DELETE_OLD_BACKUPS" == "true" ]; then
+  find "$BACKUP_DIR/" -type d -mtime +$KEEP_BACKUP_DAYS -exec rm -r {} \;
+fi
+
+sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)80\([^0-9]\)/\1$HTTP_PORT\2/" /usr/syno/share/nginx/*.mustache
+sed -i "s/^\([ \t]\+listen[ \t]\+[]:[]*\)443\([^0-9]\)/\1$HTTPS_PORT\2/" /usr/syno/share/nginx/*.mustache
 
 if which synoservicecfg; then
   synoservicecfg --restart nginx
@@ -21,5 +46,6 @@ else
   synosystemctl restart nginx
 fi
 
+echo "Made these changes:"
 
-# sed -i -e 's/50050050080/80/' -e 's/505050443/443/' /usr/syno/share/nginx/server.mustache /usr/syno/share/nginx/DSM.mustache /usr/syno/share/nginx/WWWService.mustache
+diff /usr/syno/share/nginx/ $CURRENT_BACKUP_DIR 2>&1 | tee $CURRENT_BACKUP_DIR/changes.log
